34e922afbe058cf1fb79ad971221ec816874afb6
Thomas McCavour <194552106+torxter@users.noreply.github.com>
2025-05-16 12:43:36 -0400

    Testing, playing with toys
    Tom is learning.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          This is the home page.
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-1">
+          Build a multi unit home. Imagine your community of the future. Social justice. God.
           {/* <img src={imageUrl} /> */}
           <LatestPost />
         </div>
